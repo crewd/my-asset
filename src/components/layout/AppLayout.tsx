fa64c4a8ce991165a7ebe70d6493e226fcdbff59
crewd <newspaper321@nate.com>
2022-07-23 03:09:13 +0900

    style: layout 반응형 스타일 추가

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -4,8 +4,8 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="m-auto">
       <header className="h-[60px] leading-[60px] border-b-2 border-secondary">
-        <nav className="w-[1400px] m-auto flex justify-between">
-          <h2 className="text-xl w-[10%] m-0">마이에셋</h2>
+        <nav className="max-w-[1400px] px-[20px] w-[100%] m-auto flex justify-between">
+          <h2 className="text-xl m-0">마이에셋</h2>
           <ul className="flex justify-around">
             <li className="mx-[10px]">종목 검색</li>
             <li className="mx-[10px]">관심 종목</li>
@@ -13,7 +13,7 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
           </ul>
         </nav>
       </header>
-      <main className="w-[1024px] m-auto pt-[30px]">
+      <main className="lg:w-[1024px] m-auto pt-[30px] xl:px-0 px-[20px]">
         {children}
       </main>
     </div>
