2e15f092ed6df6e7c15786c2bd47803eb5e7ad0f
crewd <newspaper321@nate.com>
2022-07-22 20:34:59 +0900

    feat: tailwind css colors, font size 커스텀

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#223144",
+        secondary: "#2D4059",
+        plus: "#13DE7C",
+        minus: "#F13B3B"
+      },
+      fontSize: {
+        regular: "16px",
+        xxl: "36px",
+        xl: "24px",
+        lg: "21px",
+        md: "18px",
+        xs: "14px",
+        xxs: "12px"
+      }
+    },
   },
   plugins: [],
 };
