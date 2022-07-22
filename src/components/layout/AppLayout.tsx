bf19f469b6f63120b9ecab51cce377fedeb54f47
crewd <newspaper321@nate.com>
2022-07-23 02:56:16 +0900

    style: layout 스타일 추가

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -2,9 +2,18 @@ import React from "react";
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="w-[1400px] m-auto">
-      <header>헤더</header>
-      <main>
+    <div className="m-auto">
+      <header className="h-[60px] leading-[60px] border-b-2 border-secondary">
+        <nav className="w-[1400px] m-auto flex justify-between">
+          <h2 className="text-xl w-[10%] m-0">마이에셋</h2>
+          <ul className="flex justify-around">
+            <li className="mx-[10px]">종목 검색</li>
+            <li className="mx-[10px]">관심 종목</li>
+            <li className="mx-[10px]">포트폴리오</li>
+          </ul>
+        </nav>
+      </header>
+      <main className="w-[1024px] m-auto pt-[30px]">
         {children}
       </main>
     </div>
