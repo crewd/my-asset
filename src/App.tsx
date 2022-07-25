e5cbdeb4db577698b9c56bf32346c49f945c7381
crewd <newspaper321@nate.com>
2022-07-25 21:10:14 +0900

    feat: react-router-dom 추가, main page 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import AppLayout from "./components/layout/AppLayout"
 import Box from "./components/layout/box/Box"
 
 function App() {
   return (
-    <AppLayout>
-      <Box classname="w-[500px] h-[500px]">마이에셋</Box>
-    </AppLayout>
+    <BrowserRouter>
+      <AppLayout>
+        <Routes>
+          <Box classname="w-[500px] h-[500px]">마이에셋</Box>
+        </Routes>
+      </AppLayout>
+    </BrowserRouter>
   )
 }
 
