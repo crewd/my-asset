5c85701a8c212acc4da37b6c2ad51a06b1479375
crewd <newspaper321@nate.com>
2022-07-25 22:12:04 +0900

    feat: main page 레이아웃 추가

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
 
@@ -13,14 +14,28 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
     <div className="m-auto">
       <header className="sm:h-[60px] leading-[60px] border-b-2 border-secondary">
         <nav className="max-w-[1400px] m-auto w-[100%] flex justify-between sm:flex-row flex-col items-start">
-          <h2 className="text-xl w-[90%] px-[20px]">마이에셋</h2>
+          <Link className="w-[90%] px-[20px]" to="/">
+            <h2 className="text-xl">마이에셋</h2>
+          </Link>
           <div className="sm:hidden absolute top-[5px] right-[20px]" onClick={menuToggle} >
             {isOpened ? <FontAwesomeIcon icon={faX} size="2x" /> : <FontAwesomeIcon icon={faBars} size="2x" />}
           </div>
           <ul className={`sm:flex ${isOpened ? "flex" : "hidden"} sm:flex-row text-center flex-col sm:justify-end items-center w-[100%]`}>
-            <li className="mx-[10px] px-[10px] sm:w-auto w-[100%]">종목 검색</li>
-            <li className="mx-[10px] px-[10px] sm:w-auto w-[100%]">관심 종목</li>
-            <li className="mx-[10px] px-[10px] sm:w-auto w-[100%]">포트폴리오</li>
+            <li className="mx-[10px] px-[10px] sm:w-auto w-[100%]">
+              <Link to="/search">
+                종목 검색
+              </Link>
+            </li>
+            <li className="mx-[10px] px-[10px] sm:w-auto w-[100%]">
+              <Link to="/favorites">
+                관심종목
+              </Link>
+            </li>
+            <li className="mx-[10px] px-[10px] sm:w-auto w-[100%]">
+              <Link to="/portfolio">
+                포트폴리오
+              </Link>
+            </li>
           </ul>
         </nav>
       </header>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,34 @@
+import Box from "../components/layout/box/Box";
+
 const MainPage = () => {
-  return <div>메인 페이지</div>
+  return (
+    <div>
+      <div className="grid grid-cols-2 gap-[10px]">
+        <Box classname="md:w-[calc(100%-10px)] h-[300px]">
+          총 자산
+        </Box>
+        <div className="grid grid-cols-2 gap-[10px]">
+          <Box classname="md:w-[calc(100%-10px)] h-[150px] col-span-2">
+            포트폴리오 바로가기
+          </Box>
+          <Box classname="md:w-[calc(100%-10px)] h-[135px]">
+            관심종목
+          </Box>
+          <Box classname="md:w-[calc(100%-10px)] h-[135px]">
+            종목 검색
+          </Box>
+        </div>
+      </div>
+      <div className="mt-[40px]">
+        <p className="p-[5px] text-center">거래량 TOP 10</p>
+        <Box classname="mt-[15px]">
+          <div className="p-[10px]">1</div>
+          <div className="p-[10px]">2</div>
+          <div className="p-[10px]">3</div>
+        </Box>
+      </div>
+    </div>
+  )
 }
 
 export default MainPage;
