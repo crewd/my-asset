bbb2b33d4932fc07a2a1da247e6fbaba2275cc50
crewd <newspaper321@nate.com>
2022-07-25 22:33:16 +0900

    style: box 컴포넌트 border 제거, 반응형 grid 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,18 +3,18 @@ import Box from "../components/layout/box/Box";
 const MainPage = () => {
   return (
     <div>
-      <div className="grid grid-cols-2 gap-[10px]">
-        <Box classname="md:w-[calc(100%-10px)] h-[300px]">
+      <div className="grid sm:grid-cols-2 gap-[10px]">
+        <Box classname="w-[100%] h-[300px] rounded-xl">
           총 자산
         </Box>
         <div className="grid grid-cols-2 gap-[10px]">
-          <Box classname="md:w-[calc(100%-10px)] h-[150px] col-span-2">
+          <Box classname="w-[100%] h-[150px] rounded-xl col-span-2">
             포트폴리오 바로가기
           </Box>
-          <Box classname="md:w-[calc(100%-10px)] h-[135px]">
+          <Box classname="w-[100%] h-[135px] rounded-xl">
             관심종목
           </Box>
-          <Box classname="md:w-[calc(100%-10px)] h-[135px]">
+          <Box classname="w-[100%] h-[135px] rounded-xl">
             종목 검색
           </Box>
         </div>
@@ -22,9 +22,9 @@ const MainPage = () => {
       <div className="mt-[40px]">
         <p className="p-[5px] text-center">거래량 TOP 10</p>
         <div className="mt-[15px]">
-          <Box classname="p-[15px] rounded-none rounded-t-lg border-b-2 border-primary">first</Box>
-          <Box classname="p-[15px] rounded-none border-b-2 border-primary">2</Box>
-          <Box classname="p-[15px] rounded-none rounded-b-lg">last</Box>
+          <Box classname="p-[15px] rounded-t-lg border-b-2 border-primary">first</Box>
+          <Box classname="p-[15px] border-b-2 border-primary">2</Box>
+          <Box classname="p-[15px] rounded-b-lg">last</Box>
         </div>
       </div>
     </div>
