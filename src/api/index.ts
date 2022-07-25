d1530f0c2dc4ff371d7364e66be109825a0a3ff6
charade6 <charade6@naver.com>
2022-07-26 00:36:35 +0900

    feat: api추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
-const baseURL = 'API주소';
+const baseURL =
+  'https://api.odcloud.kr/api/GetStockSecuritiesInfoService/v1/getStockPriceInfo?resultType=json';
+
+const serviceKey = `gIgWSDzCeTDpTHNna3UfLVrfBmHbLPDu8IRh%2FvJuoHy5Sp1OFCc9r6uWHIqcEpCF8pWmul9zZMDQLafiKcrx3Q%3D%3D`;
 
 export const getData = async (stockName: string) => {
-  const data = axios.get(`${baseURL}/요청주소${stockName}`);
-  return data;
+  const data = axios.get(
+    `${baseURL}&likeItmsNm=${stockName}&serviceKey=${serviceKey}`,
+  );
+
+  return (await data).data.response.body.items.item;
 };
