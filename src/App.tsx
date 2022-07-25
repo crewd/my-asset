28a4ea6d02aca2f2317ff3e5adf1d95ef847fcdf
crewd <newspaper321@nate.com>
2022-07-25 21:18:48 +0900

    feat: page path 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import AppLayout from "./components/layout/AppLayout"
 import Box from "./components/layout/box/Box"
+import MainPage from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
       <AppLayout>
         <Routes>
-          <Box classname="w-[500px] h-[500px]">마이에셋</Box>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/favorites" />
+          <Route path="/portfolio" />
+          <Route path="/portfolio/:id" />
+          <Route path="/search" />
         </Routes>
       </AppLayout>
     </BrowserRouter>
