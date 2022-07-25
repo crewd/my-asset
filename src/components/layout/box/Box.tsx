683a69ea2804ba312c8c52aae5119cc0cf482af5
crewd <newspaper321@nate.com>
2022-07-25 17:37:39 +0900

    style: Box shadow 추가

--- src/components/layout/box/Box.tsx
+++ src/components/layout/box/Box.tsx
@@ -1,5 +1,5 @@
 const Box = ({ children, classname }: { children: React.ReactNode, classname: string }) => {
-  const styles = `bg-secondary rounded-lg p-[10px] ${classname}`
+  const styles = `bg-secondary rounded-lg p-[10px] shadow-xl ${classname}`
 
   return (
     <div className={styles}>
