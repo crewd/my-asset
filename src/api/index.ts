5fbd5fbfbc62f7b0a5a7b109571b386d64a81e89
crewd <newspaper321@nate.com>
2022-07-27 04:28:55 +0900

    fix: await 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,9 +6,9 @@ const baseURL =
 const serviceKey = `gIgWSDzCeTDpTHNna3UfLVrfBmHbLPDu8IRh%2FvJuoHy5Sp1OFCc9r6uWHIqcEpCF8pWmul9zZMDQLafiKcrx3Q%3D%3D`;
 
 export const getStockData = async (stockName: string) => {
-  const data = axios.get(
+  const data = await axios.get(
     `${baseURL}&likeItmsNm=${stockName}&serviceKey=${serviceKey}`,
   );
 
-  return (await data).data.response.body.items.item;
+  return data.data.response.body.items.item;
 };
