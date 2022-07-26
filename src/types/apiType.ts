6f3ad9ade82bd916612f2f7debce3392c0a3cc5c
charade6 <charade6@naver.com>
2022-07-26 21:31:07 +0900

    feat: api타입 추가

---
+++ src/types/apiType.ts
@@ -0,0 +1,17 @@
+export type Stock = {
+  basDt: string;
+  srtnCd: string;
+  isinCd: string;
+  itmsNm: string;
+  mrktCtg: string;
+  clpr: string;
+  vs: string;
+  fltRt: string;
+  mkp: string;
+  hipr: string;
+  lopr: string;
+  trqu: string;
+  trPrc: string;
+  lstgStCnt: string;
+  mrktTotAmt: string;
+};
