644282d8544520fdd2f68b72f71a43ca4e85dec2
crewd <newspaper321@nate.com>
2022-07-27 02:20:37 +0900

    feat: QueryClient 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AppLayout from './components/layout/AppLayout';
 import MainPage from './pages/MainPage';
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        refetchOnReconnect: false,
+      },
+    },
+  });
   return (
-    <BrowserRouter>
-      <AppLayout>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/favorites" />
-          <Route path="/portfolio" />
-          <Route path="/portfolio/:id" />
-          <Route path="/search" />
-        </Routes>
-      </AppLayout>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AppLayout>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/favorites" />
+            <Route path="/portfolio" />
+            <Route path="/portfolio/:id" />
+            <Route path="/search" />
+          </Routes>
+        </AppLayout>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
