fa12f18aeb35c3754d67337f9c447851c6f11dcc
crewd <newspaper321@nate.com>
2022-07-28 02:24:32 +0900

    fix: list key값 추가

--- src/components/box/List.tsx
+++ src/components/box/List.tsx
@@ -4,7 +4,9 @@ const List = ({ data }: { data: (string | number)[] }) => {
   return (
     <Box classname="flex justify-between sm:p-[15px] p-[10px] first:rounded-t-xl last:rounded-b-xl border-b-2 border-primary text-center text-sm sm:text-regular">
       {data.map((data) => (
-        <p className="w-full">{data}</p>
+        <p key={data} className="w-full">
+          {data}
+        </p>
       ))}
     </Box>
   );
