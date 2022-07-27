6c9da6946410637492768d33ce609b43af1a6765
charade6 <charade6@naver.com>
2022-07-27 16:12:10 +0900

    feat: Button Component 추가

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,13 @@
+function Button({
+  children,
+  classname,
+}: {
+  children: React.ReactNode;
+  classname?: string;
+}) {
+  const styles = `block bg-secondary shadow-xl m-auto mt-[18px] py-[8px] px-[48px] rounded-lg hover:bg-[#527099] transition-colors duration-300 ${classname}`;
+
+  return <button className={styles}>{children}</button>;
+}
+
+export default Button;
