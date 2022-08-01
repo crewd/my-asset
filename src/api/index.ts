ae122f5d4669f3d4358f4452c81f416dfb466869
charade6 <charade6@naver.com>
2022-08-01 22:09:04 +0900

    feat: api 날짜 요청변수 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,9 +6,19 @@ const baseURL =
 const serviceKey =
   'gIgWSDzCeTDpTHNna3UfLVrfBmHbLPDu8IRh%2FvJuoHy5Sp1OFCc9r6uWHIqcEpCF8pWmul9zZMDQLafiKcrx3Q%3D%3D';
 
+const calcDate = () => {
+  const today = new Date();
+  const weekAgo = new Date(today.setDate(today.getDate() - 7))
+    .toLocaleDateString()
+    .split(/[ .]/g)
+    .filter((f) => f.length !== 0);
+
+  return weekAgo.map((parm) => (parm.length > 1 ? parm : 0 + parm)).join('');
+};
+
 export const getStockData = async (stockName: string) => {
   const data = await axios.get(
-    `${baseURL}&likeItmsNm=${stockName}&serviceKey=${serviceKey}`,
+    `${baseURL}&likeItmsNm=${stockName}&serviceKey=${serviceKey}&beginBasDt=${calcDate()}`,
   );
 
   return data.data.response.body.items.item;
@@ -16,7 +26,7 @@ export const getStockData = async (stockName: string) => {
 
 export const stockCodeSearch = async (code: number | string) => {
   const data = await axios.get(
-    `${baseURL}&likeSrtnCd=${code}&serviceKey=${serviceKey}`,
+    `${baseURL}&likeSrtnCd=${code}&serviceKey=${serviceKey}&beginBasDt=${calcDate()}`,
   );
 
   return data.data.response.body.items.item[0];
