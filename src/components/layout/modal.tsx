94c1be1d1bdbe028b32ec652c46610297d76c5d3
crewd <newspaper321@nate.com>
2022-08-05 04:58:41 +0900

    feat: modal 컴포넌트 추가

---
+++ src/components/layout/modal.tsx
@@ -0,0 +1,4 @@
+import { createPortal } from 'react-dom';
+
+export const ModalPortal = ({ children }: { children: React.ReactNode }) =>
+  createPortal(children, document.getElementById('modal')!);
