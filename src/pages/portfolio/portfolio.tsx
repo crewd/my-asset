8d4f2d4e62307b00e727ccd96e2b10a33ca13463
crewd <newspaper321@nate.com>
2022-08-06 05:19:16 +0900

    design: portfolio.tsx css 변경

--- src/pages/portfolio/portfolio.tsx
+++ src/pages/portfolio/portfolio.tsx
@@ -44,74 +44,70 @@ const Portfolios = () => {
   }, []);
 
   return (
-    <div>
-      <div className="grid grid-cols-1 gap-[20px]">
-        <div className="m-auto sm:w-[600px] w-full">
-          <Box classname=" h-[250px] rounded-xl sm:p-[30px] p-[25px] flex justify-between sm:flex-row flex-col">
-            <div className="w-full">
-              <div>
-                <p className="text-lg">총 보유 자산</p>
-                <p className="sm:text-xxl text-[32px] font-bold">
-                  {totalAmount.toLocaleString()}원
-                </p>
-              </div>
-              <div className="mt-8 flex justify-between">
-                <p className="text-md">수익률</p>
-                <p
-                  className={`font-bold text-lg ${
-                    returnRate < 0
-                      ? 'text-minus'
-                      : returnRate > 0
-                      ? 'text-plus'
-                      : 'text-white'
-                  }`}
-                >
-                  {Number(returnRate).toFixed(2)}%
-                </p>
-              </div>
-              <div className="mt-[10px] flex justify-between">
-                <p className="text-md">평가 손익</p>
-                <p
-                  className={`font-bold text-lg ${
-                    profit < 0
-                      ? 'text-minus'
-                      : profit > 0
-                      ? 'text-plus'
-                      : 'text-white'
-                  }`}
-                >
-                  {profit ? profit.toLocaleString() : 0}원
-                </p>
-              </div>
+    <div className="m-auto sm:w-[600px] w-full">
+      <Box classname=" mb-[24px] h-[250px] rounded-xl sm:p-[30px] p-[25px] flex justify-between sm:flex-row flex-col">
+        <div className="w-full">
+          <div>
+            <p className="text-lg">총 보유 자산</p>
+            <p className="sm:text-xxl text-[32px] font-bold">
+              {totalAmount.toLocaleString()}원
+            </p>
+          </div>
+          <div className="mt-8 flex justify-between">
+            <p className="text-md">수익률</p>
+            <p
+              className={`font-bold text-lg ${
+                returnRate < 0
+                  ? 'text-minus'
+                  : returnRate > 0
+                  ? 'text-plus'
+                  : 'text-white'
+              }`}
+            >
+              {Number(returnRate).toFixed(2)}%
+            </p>
+          </div>
+          <div className="mt-[10px] flex justify-between">
+            <p className="text-md">평가 손익</p>
+            <p
+              className={`font-bold text-lg ${
+                profit < 0
+                  ? 'text-minus'
+                  : profit > 0
+                  ? 'text-plus'
+                  : 'text-white'
+              }`}
+            >
+              {profit ? profit.toLocaleString() : 0}원
+            </p>
+          </div>
+        </div>
+      </Box>
+      <div className="m-auto sm:w-[600px] w-[100%]">
+        <p className="py-[10px] text-md">내 포트폴리오</p>
+        <div>
+          <Box classname="m-auto sm:w-[600px] w-full h-[300px] p-[20px] rounded-t-lg border-b-2 border-primary text-black">
+            <div className="sm:w-full w-[200px] h-full m-auto">
+              {chartData.length > 0 && <MyResponsivePie data={chartData} />}
             </div>
           </Box>
+          {myStockData.length > 0 ? (
+            myStockData.map((element) => (
+              <PortfolioCard
+                classname="bg-secondary last:rounded-b-xl last:border-none border-b-2 border-primary sm:p-[20px] p-[15px]"
+                key={element.name}
+                name={element.name}
+                stock={element.holdingStock}
+                navigate={() => navigateHandler(element.id)}
+              />
+            ))
+          ) : (
+            <List data={['포트폴리오를 추가해보세요!']} />
+          )}
         </div>
-        <div className="m-auto sm:w-[600px] w-[100%]">
-          <p className="py-[10px] text-md">내 포트폴리오</p>
-          <div>
-            <Box classname="m-auto sm:w-[600px] w-full h-[300px] p-[20px] rounded-t-lg border-b-2 border-primary text-black">
-              <div className="sm:w-full w-[200px] h-full m-auto">
-                {chartData.length > 0 && <MyResponsivePie data={chartData} />}
-              </div>
-            </Box>
-            {myStockData.length > 0 ? (
-              myStockData.map((element) => (
-                <PortfolioCard
-                  classname="bg-secondary last:rounded-b-xl last:border-none border-b-2 border-primary sm:p-[20px] p-[15px]"
-                  key={element.name}
-                  name={element.name}
-                  stock={element.holdingStock}
-                  navigate={() => navigateHandler(element.id)}
-                />
-              ))
-            ) : (
-              <List data={['포트폴리오를 추가해보세요!']} />
-            )}
-          </div>
-          <Button classname="">
-            <FontAwesomeIcon icon={faPlus} size="lg" />
-          </Button>
-        </div>
+        <Button classname="">
+          <FontAwesomeIcon icon={faPlus} size="lg" />
+        </Button>
       </div>
     </div>
   );
