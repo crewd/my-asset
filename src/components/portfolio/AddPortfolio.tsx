0ddc15bd9c2bafed69971eeb09ffaff540611412
crewd <newspaper321@nate.com>
2022-08-09 16:32:30 +0900

    feat: useRef 추가

--- src/components/portfolio/AddPortfolio.tsx
+++ src/components/portfolio/AddPortfolio.tsx
@@ -1,7 +1,11 @@
-import { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { stockStore } from '../../util/stock';
 import { ModalPortal } from '../layout/modal';
 
 const AddPortfolio = ({ cancel }: { cancel: () => void }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const store = stockStore;
+
   useEffect(() => {
     document.body.style.cssText = `
     position: fixed; 
@@ -20,12 +24,14 @@ const AddPortfolio = ({ cancel }: { cancel: () => void }) => {
       <div className="fixed top-0 left-0 bottom-0 right-0 bg-[rgba(0,0,0,0.5)] before:absolute before:top-0 before:left-0 before:bottom-0 before:right-0 before:content-none">
         <div className="absolute top-[50%] left-[50%] p-[20px] w-[80%] translate-x-[-50%] translate-y-[-50%] max-w-[600px] bg-secondary rounded-xl">
           <div className="my-[20px]">
-            <p className="py-[10px] text-md">포트폴리오 이름</p>
+            <p className="pb-[10px] text-md">포트폴리오 이름</p>
             <input
               className="w-full bg-primary outline-none p-[10px] rounded-md"
               type="text"
               maxLength={10}
               placeholder="10자 이하"
+              ref={inputRef}
+              onChange={() => console.log(inputRef.current?.value)}
             />
           </div>
           <div className="w-full flex justify-center">
