7796c8d94e2f7acf75e63899439f3b131e76227a
crewd <newspaper321@nate.com>
2022-08-10 01:38:31 +0900

    refactor: modal 컴포넌트 변경

--- src/components/layout/modal.tsx
+++ src/components/layout/modal.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-export const ModalPortal = ({ children }: { children: React.ReactNode }) => {
+const ModalPortal = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
     document.body.style.cssText = `
     position: fixed; 
@@ -17,3 +17,15 @@ export const ModalPortal = ({ children }: { children: React.ReactNode }) => {
 
   return createPortal(children, document.getElementById('modal'));
 };
+
+const Modal = ({ children }: { children: React.ReactNode }) => (
+  <ModalPortal>
+    <div className="fixed top-0 left-0 bottom-0 right-0 bg-[rgba(0,0,0,0.5)] before:absolute before:top-0 before:left-0 before:bottom-0 before:right-0 before:content-none">
+      <div className="absolute top-[50%] left-[50%] p-[20px] w-[80%] translate-x-[-50%] translate-y-[-50%] max-w-[600px] bg-secondary rounded-xl">
+        {children}
+      </div>
+    </div>
+  </ModalPortal>
+);
+
+export default Modal;
--- src/components/portfolio/AddPortfolio.tsx
+++ src/components/portfolio/AddPortfolio.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { myStockState } from '../../recoils/stock';
 import { stockStore } from '../../util/stock';
-import { ModalPortal } from '../layout/modal';
+import Modal from '../layout/modal';
 
 const AddPortfolio = ({ cancel }: { cancel: () => void }) => {
   const [inputValue, setInputValue] = useState('');
@@ -33,38 +33,34 @@ const AddPortfolio = ({ cancel }: { cancel: () => void }) => {
   }, [myStockData]);
 
   return (
-    <ModalPortal>
-      <div className="fixed top-0 left-0 bottom-0 right-0 bg-[rgba(0,0,0,0.5)] before:absolute before:top-0 before:left-0 before:bottom-0 before:right-0 before:content-none">
-        <div className="absolute top-[50%] left-[50%] p-[20px] w-[80%] translate-x-[-50%] translate-y-[-50%] max-w-[600px] bg-secondary rounded-xl">
-          <div className="my-[20px]">
-            <p className="pb-[10px] text-md">포트폴리오 이름</p>
-            <input
-              className="w-full bg-primary outline-none p-[10px] rounded-md"
-              type="text"
-              maxLength={10}
-              placeholder="10자 이하"
-              onChange={onChangeHandler}
-            />
-          </div>
-          <div className="w-full flex justify-center">
-            <button
-              className="bg-secondray w-[100px] h-[50px] mx-[10px] rounded-md border-2 border-primary hover:bg-primary shadow-xl"
-              type="button"
-              onClick={cancel}
-            >
-              닫기
-            </button>
-            <button
-              className="bg-secondray w-[100px] h-[50px] mx-[10px] rounded-md border-2 border-primary hover:bg-primary shadow-xl"
-              type="button"
-              onClick={addHandler}
-            >
-              추가
-            </button>
-          </div>
-        </div>
+    <Modal>
+      <div className="my-[20px]">
+        <p className="pb-[10px] text-md">포트폴리오 이름</p>
+        <input
+          className="w-full bg-primary outline-none p-[10px] rounded-md"
+          type="text"
+          maxLength={10}
+          placeholder="10자 이하"
+          onChange={onChangeHandler}
+        />
       </div>
-    </ModalPortal>
+      <div className="w-full flex justify-center">
+        <button
+          className="bg-secondray w-[100px] h-[50px] mx-[10px] rounded-md border-2 border-primary hover:bg-primary shadow-xl"
+          type="button"
+          onClick={cancel}
+        >
+          닫기
+        </button>
+        <button
+          className="bg-secondray w-[100px] h-[50px] mx-[10px] rounded-md border-2 border-primary hover:bg-primary shadow-xl"
+          type="button"
+          onClick={addHandler}
+        >
+          추가
+        </button>
+      </div>
+    </Modal>
   );
 };
 
